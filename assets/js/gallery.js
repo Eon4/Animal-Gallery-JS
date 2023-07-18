5d694cb4ec60d4ddc6b43fe50a02b4cfb64bdc7e
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-07-18 22:54:10 +0200

    missing code fixed

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,10 +1,12 @@
 
 
-/*delay på data hentning i ms */
+/*delay på data hentning i ms for at simulere data hentning online*/
 const myLoadTime = 2000;
 
 // reset variables
 let myData = null;
+
+let myApp = null;
 
 
 
@@ -12,14 +14,130 @@ let myData = null;
 window.addEventListener("load", initApp);
 
 
-// din kode her 
+function initApp() {
+    fetchData(); // starter hentning af data. 
+    myApp = document.getElementById('app'); // vi finder det tag voress app skal leve i. kaldet app
+    createLoadingScreen(); // bygger loading screen i DOM.
+}
 
 
+function createLoadingScreen() {
+    // byg loading screen her med html dom elementer. og evt et animeret gif eller billede. i vores app tag.
+    //SECTION TIL LOADING ICON
+    let loadingScreenWrapper = document.createElement('section');
+    loadingScreenWrapper.classList.add('loadingWrapper');
+    myApp.appendChild(loadingScreenWrapper);
+    //LOADING ICON
+    let loadingIcon = document.createElement('img');
+    loadingIcon.src = './assets/img/loading.png';
+    loadingIcon.classList.add('loadingIcon');
+    loadingScreenWrapper.appendChild(loadingIcon);
+}
 
+
+function initGallery(dataRecived) {
+
+    // kaldes fra fetchData når data er klar. 
+    // set myData variablen til det modtagne data, så det er tilgængelig for alle funktioner
+    myData = dataRecived;
+    //kald funktionen resetGallery for at slette indhold i app tagget, som er indeholdt i myApp.
+    resetGallery(dataRecived);
+    // kald en funktion der kan bygge dit galleri. den hedder buildGallery
+    buildGallery(dataRecived);
+    // console.log('Her er dataen', dataRecived);
+}
+
+
+function resetGallery(data) {
+    // skriv kode her der kan slette alt html i app tagget husk det er indeholdt i  myApp
+    myApp.innerHTML = "";
+}
+
+function buildGallery(leData) {
+    // console.log('Here is the data', leData);
+    /* brug map funktionen paa vores myData for at finde data for hvert enkelt dyr, og sende det til en funktion der
+    kan bygge dit galleri kort for dyret. funktionen hedder buildCard, og har brugfor data for dyret*/
+    leData.map((animals) =>{
+        // console.log('wuts dis?', animals);
+        buildCard(animals);
+        console.log(animals);
+    });
+    
+}
+
+
+function buildCard(myAnimalData) {
+    // console.log(myAnimalData);
+    /* skriv kode der kan vise data fra myAnimalData i DOM
+    husk at bruge createElement og appendChild funktionerne til at bygge semantisk korrekt HTML (se evt codelab om dom elementer opgave 4)
+    */
+
+    //CARD
+    let galleryWrapper = document.createElement('section');
+    galleryWrapper.classList.add('galleryCard');
+    myApp.appendChild(galleryWrapper);
+
+    //NAME
+    let animalName = document.createElement('h2');
+    animalName.innerText = myAnimalData.name;
+    galleryWrapper.appendChild(animalName);
+
+    //PICTURE
+    let animalImg = document.createElement('img');
+    animalImg.src = myAnimalData.picture;
+    animalImg.classList.add('galleryCardImg');
+    galleryWrapper.appendChild(animalImg);
+
+    //SHORT DESCRIPTION
+    let desShort = document.createElement('p');
+    desShort.innerText = myAnimalData.shortDescription;
+    galleryWrapper.appendChild(desShort);
+
+    //MODAL
+    let modal = document.createElement('dialog');
+    modal.classList.add('modalDialog');
+    myApp.appendChild(modal);
+
+    //TITLE TIL MODAL
+    let modalTitle = document.createElement('h2');
+    modalTitle.innerText = myAnimalData.name;
+    modalTitle.classList.add('modalTitle');
+    modal.appendChild(modalTitle);
+
+    //PICTURE TIL MODAL
+    let modalImg = document.createElement('img');
+    modalImg.src = myAnimalData.picture;
+    modalImg.classList.add('detailViewImg');
+    modal.appendChild(modalImg);
+    
+    // //ESC TEKST
+    // let escText = document.createElement('p');
+    // escText.innerText = 'Tryk på Esc for at lukke'
+    // escText.classList.add('escText');
+    // modal.appendChild(escText);
+
+    //LONG DESCRIPTION TIL MODAL
+    let desLong = document.createElement('p');
+    desLong.innerText = myAnimalData.description;
+    desLong.classList.add('longDesc');
+    modal.appendChild(desLong);
+
+    //VISER MODAL
+    galleryWrapper.addEventListener("pointerup", (event)=> {
+        // console.log('Der blev klikket.', event);
+        modal.showModal();
+    });
+
+    //SKJULER MODAL (ikke pt...)
+    modal.addEventListener("pointerup", (event)=> {
+        // console.log('Vi er i close eventet' + event.currentTarget);
+        modal.close();
+
+    });
+}
 
 
 /*  get data function  DO NOT TOUCH!!!!! ......................................................
-
 denne funktion vil typisk være en funktion der henter data fra et API
 */
 
@@ -27,7 +145,7 @@ denne funktion vil typisk være en funktion der henter data fra et API
 
 async function fetchData() {
     // data object
-    console.log('fetching data');
+    // console.log('fetching data');
     await new Promise(resolve => setTimeout(resolve, myLoadTime));
 
     const myData = [
@@ -71,4 +189,3 @@ async function fetchData() {
 
 
 }
-
